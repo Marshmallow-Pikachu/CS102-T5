55d0ad845d6593e641d9856da85a249781274845
Ada <j.boomuang.2024@smu.edu.sg>
2025-03-05 15:12:17 +0800

    Update Card class with all the necessary attributes and methods

--- app/resource/Card.java
+++ app/resource/Card.java
@@ -1,5 +1,43 @@
 package app.resource;
 
 public class Card {
-    
+    private int value;
+    private String colour;
+    private boolean removalMode;
+    private boolean flipped;
+
+    public Card(int value, String colour) {
+        this.value = value;
+        this.colour = colour;
+        removalMode = false;
+        flipped = false;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public boolean getRemovalMode(){
+        return removalMode;
+    }
+
+    public boolean getFlipped() {
+        return flipped;
+    }
+
+    public void setRemovalMode(boolean removalMode) {
+        this.removalMode = removalMode;
+    }
+
+    public void setFlipped (boolean flipped) {
+        this.flipped = flipped;
+    }
+
+    public String toString() {
+        return "Colour: " + colour + " Number: " + value;
+    }
 }
