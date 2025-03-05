747387ffd88ef7cc90a621c946c183c1a2eab678
Ada <j.boomuang.2024@smu.edu.sg>
2025-03-05 15:26:57 +0800

    Update Deck class only including the structure (have not worked on the functioning of each method)

--- app/resource/Deck.java
+++ app/resource/Deck.java
@@ -1,5 +1,24 @@
 package app.resource;
 
+import java.util.*;
 public class Deck {
-    
+    private ArrayList<Card> cards;
+    private boolean isEmpty;
+
+    public Deck () {
+        // initialise ArrayList<Card>
+    }
+
+    public boolean getIsEmpty() {
+        return isEmpty;
+    }
+
+    public Card drawCard() {
+        // It should return a Card and remove that from its own array list
+        return null;
+    }
+
+    public void shuffle() {
+
+    }
 }
