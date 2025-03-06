8783b4255a1a3fd3b3387c4818a38a518f945baa
Sonia Poh <soniapoh@gmail.com>
2025-03-06 21:57:28 +0800

    Updated Parade class with all methods and attributes

--- app/resource/Parade.java
+++ app/resource/Parade.java
@@ -4,14 +4,30 @@ import java.util.*;
 
 public class Parade {
     private ArrayList<Card> paradeCards;
-    
-    public int getNumberOfCardsInParade() {
-        return this.paradeCards.size();
+
+    public Parade(ArrayList<Card> paradeCards) {
+        this.paradeCards = paradeCards;
     }
-    public ArrayList<Card> getParadeCards() {
-        return this.paradeCards;
+
+    public ArrayList<Card> removeEligibleCards(Card playedCard) {
+        ArrayList<Card> removableCardsFromParadeList = new ArrayList<>(); //list of cards that are removed from parade
+        for (Card currentParadeCard:paradeCards) {
+            // checking eligibility of cards to be removed from Parade to Players' Hand
+            if (currentParadeCard.getValue() <= playedCard.getValue() || currentParadeCard.getColour().equals(playedCard.getColour())) {
+                removableCardsFromParadeList.add(currentParadeCard); 
+                paradeCards.remove(currentParadeCard);  
+            }
+        }
+        return removableCardsFromParadeList;
     }
-    public void removeEligibleCard(Card card) {
-        
+
+    public void addToParade(Card playedCard) {
+        paradeCards.add(playedCard); // add playedCard to paradeCards
+    }
+
+    public void displayParade() {
+        for(Card card:paradeCards) {
+            System.out.println(card.toString());
+        }
     }
 }
