5b40c3c29255e8e59033ee7999cc973e68f5b306
Jeffrey <jeffreyng2002@gmail.com>
2025-03-12 03:40:05 +0800

    implemented everything except flip for 2 players

--- src/app/game/Game.java
+++ src/app/game/Game.java
@@ -1,11 +1,8 @@
 package app.game;
 
-import java.util.*;
-
 import app.entity.*;
 import app.resource.*;
-import app.utilities.*;
-import java.lang.reflect.Array;
+import java.util.*;
 
 
 //import Parade/Players when done can use the above as example 
@@ -18,13 +15,13 @@ public class Game {
     // Declaring ANSI_RESET so that we can reset the color 
     public static final String ANSI_RESET = "\u001B[0m"; 
   
-    // Declaring the color (Red, Blue, Purple, Green, Grey, Orange) 
-    public static final String ANSI_BLACK  = "\u001B[30m";
+    // Declaring the color (Red, Blue, Purple, Green, Black, Yellow) 
     public static final String ANSI_RED    = "\u001B[31m";
     public static final String ANSI_BLUE   = "\u001B[34m";
-    public static final String ANSI_YELLOW = "\u001B[33m"; 
-    public static final String ANSI_BRIGHT_GREEN  = "\u001B[92m";
     public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_BRIGHT_GREEN  = "\u001B[92m";
+    public static final String ANSI_BLACK  = "\u001B[30m";
+    public static final String ANSI_YELLOW = "\u001B[33m"; 
 
     // Feel free to add to psvm to check if your parts integrate
     private ArrayList<Player> players;
@@ -45,13 +42,21 @@ public class Game {
     // }
 
 
-    public Game(ArrayList<String> playersName){
+    public Game(ArrayList<String> playersName, boolean b){
         deck = new Deck();
         parade = new Parade(deck);
         players = new ArrayList<Player>();
         for (String s : playersName){ //Add Player into ArrayList
             players.add(makeHumanPlayer(s));
         }
+        System.out.println("Game Started");
+    }
+
+
+    public Game(ArrayList<Player> players){
+        deck = new Deck();
+        parade = new Parade(deck);
+        this.players = players;
         System.out.println("Game Started");
     }
 
@@ -257,10 +262,18 @@ public class Game {
 // If the draw pile is exhausted, every player plays one more turn. The game ends when everybody has only 4 cards left in their hand.
 // Even if other players get their 6th color during the last round, this has no further effect. The games ends after the round.
 
-    public void initiateFinalRound(){ // if a player has all 6 colors, or deck is empty
+    public void initiateFinalRound(ArrayList<Player> players){ // if a player has all 6 colors, or deck is empty
         //Starts off with the person who initiated last round
         // Reorder the players ArrayList? / Make another ArrayList with Person who started last round as first man? / Any other ideas?
 
+        System.out.println("Final Round!!");
+        for (Player p : players){
+            p.takeTurn(new Deck(), parade); // Change to p.takeTurnWithoutDrawing
+        }
+        //System.out.println("Test1");
+        flipMajorityCards();
+        //System.out.println("Test2");
+        //discardTwoCards(players);
     }
 
 
@@ -269,7 +282,7 @@ public class Game {
     }
 
 
-// After the end of the game each player chooses 2 cards from their hand and discardsthem. 
+// After the end of the game each player chooses 2 cards from their hand and discards them. 
 // The remaining 2 cards in hand are added to those already in front of the player.
 // Note: Each of these 2 cards will either be added to colors you already own, or create a new pile (if you do not already have any cards of that color).
 // Only the cards lying in front of a player are scored. The cards still taking part in the parade are discarded.
@@ -282,17 +295,211 @@ public class Game {
 // 2.  Each player will then add up the printed values of all their face up cards. Sum up
 // this total with the total obtained from any face down cards.
 
-    public void flipMajorityCards(){  // make sure to check if it’s a two player game
-        if (players.size() == 2){
+    public ArrayList<Integer> getCountOfColors(ArrayList<Card> cards){
+        ArrayList<Integer> count = new ArrayList<>(6);
+        count.add(0);count.add(0);count.add(0);count.add(0);count.add(0);count.add(0);
+        for (Card c : cards){
+            switch (c.getColour()){
+                case ("Red"):
+                count.set(0, count.get(0)+1);
+                break;
+                case ("Blue"):
+                count.set(1, count.get(1)+1);
+                break;
+                case ("Purple"):
+                count.set(2, count.get(2)+1);
+                break;
+                case ("Green"):
+                count.set(3, count.get(3)+1);
+                break;
+                case ("Black"):
+                count.set(4, count.get(4)+1);
+                break;
+                case ("Yellow"):
+                count.set(5, count.get(5)+1);
+                break;
+            }
+        }
+        return count;
+    }
 
-        } else{
+    public void flipCardsByColour(ArrayList<Card> cardList,int val){
+        String colour = null;
+        switch (val){
+            case 0:
+            colour = "Red";
+            break;
+            case 1:
+            colour = "Blue";
+            break;
+            case 2:
+            colour = "Purple";
+            break;
+            case 3: 
+            colour = "Green";
+            break;
+            case 4: 
+            colour = "Black";
+            break;
+            case 5: 
+            colour = "Yellow";
+            break;
+        }
+        //System.out.println("Colour: "+colour);
+        for (Card c : cardList){
+            //System.out.println(c.getColour());
+            if (c.getColour().equals(colour)){
+                //System.out.println("FLIPPPPPPPPPP");
+                c.setFlipped();
+            }
+        }
+    }
+
+    public ArrayList<Integer> getCountOfColour(ArrayList<Card> cardList, ArrayList<String> colourList){
+        ArrayList<Integer> returnList = new ArrayList<>();
+        returnList.add(0);returnList.add(0);returnList.add(0);returnList.add(0);returnList.add(0);returnList.add(0);
+        for (Card c : cardList){
+            for (int i = 0; i < 6; i++){
+                if (c.getColour().equals(colourList.get(i))){
+                    returnList.set(i, returnList.get(i) + 1);
+                }
+            }
+        }
+        return returnList;
+    }
+
+
+    public void flipMajorityCards(){  // make sure to check if it’s a two player game
+        if (players.size() == 2){ // 2 Players
+            ArrayList<Card> p1 = players.get(0).getCollectedParadeCards();
+            ArrayList<Card> p2 = players.get(1).getCollectedParadeCards();
+            ArrayList<Integer> player1 = getCountOfColors(p1);
+            ArrayList<Integer> player2 = getCountOfColors(p2);
+            //System.out.println("TEST1: 2 Players HIT");
+            for (int i = 0; i < 5; i++){
+                int diff = player1.get(i) - player2.get(i);
+                if (diff >= 2){
+                    flipCardsByColour(p1, i);
+                }else if (diff <= -2){
+                    flipCardsByColour(p2, i);
+                }
+            }
+        } 
+        else{
+            System.out.println("TEST1: MORE THAN 2 Players HIT");
+            ArrayList<String> colourList = new ArrayList<>();
+            colourList.add("Red");colourList.add("Blue");colourList.add("Purple");colourList.add("Green");colourList.add("Black");colourList.add("Yellow");
+            ArrayList<Integer> colourCount = new ArrayList<>(6);
+            colourCount.add(0);colourCount.add(0);colourCount.add(0);colourCount.add(0);colourCount.add(0);colourCount.add(0);
+
+
+            //System.out.println("TEST2");
+            for (Player p : players){ //Get Majority of Each Colour
+                ArrayList<Card> collectedCards = p.getCollectedParadeCards();
+
+                //Index 0: Red, Index 1: Blue, Index 2: Purple, Index 3: Green, Index 4: Black, Index 5, Yellow
+                
+                List<Integer> counts = new ArrayList<>(); //Gets Number of Each Color in Hand
+                for (String colour : colourList) {
+                    long count = collectedCards.stream()
+                                            .filter(card -> colour.equals(card.getColour()))
+                                            .count();
+                    counts.add((int)count);
+                }
+                // System.out.println("SIZE: " + counts.size());
+                // System.out.println("SIZE2: " + colourCount.size());
+
+                for (int i = 0; i < 6; i++){ //Populate colourCount with MAX no. of colour in game
+                    if (colourCount.get(i) < counts.get(i)){ //HERE!
+                        colourCount.set(i, counts.get(i));
+                    }
+                }
+            }
+            System.out.println("Majority of Each Colour");
+            for (int i = 0; i < 6; i++){
+                System.out.println("Count of : " + colourList.get(i) + " = " + colourCount.get(i));
+            }
+
+            for (Player p: players){ //do flippings using the Majority of Each Colour
+                //System.out.println(p.getName());
+                ArrayList<Card> scoringZone = p.getCollectedParadeCards();
+                //System.out.println(p.getName());
+                ArrayList<Integer> numOfColours = getCountOfColors(scoringZone);
+                // for (Integer i : numOfColours){
+                //     System.out.println(i);
+                // }
+                // System.out.println("numOfColours : " + numOfColours);
+                // System.out.println("numOfColours : " + colourCount);
+                for (int i = 0; i < 6; i++){
+                    if (numOfColours.get(i) == colourCount.get(i)){
+                        //System.out.println("Flip for : " + p.getName() + " Colour: "+colourList.get(i) + " Count : " + numOfColours.get(i));
+                        flipCardsByColour(scoringZone, i);
+                    }
+                }
+            }
+
+            //System.out.println("TEST3");
+            // for (Player p: players){ //do flippings using the Majority of Each Colour
+            //     System.out.println(p.getName());
+            //     ArrayList<Card> scoringZone = p.getCollectedParadeCards();
+            //     for (Card c : scoringZone){
+            //         int i = 0;
+
+
+            //         for (String colour : colourList){
+            //             int countOfColour = (int) scoringZone.stream()
+            //                                 .filter(card -> colour.equals(c.getColour()))
+            //                                 .count();
+
+
+            //             //System.out.println("HAHA");
+            //             System.out.println("Count of "+colour + " : " + countOfColour);
+            //             System.out.println("Count of MAX " + colour + " : " + colourCount.get(i));
+            //             if (colourCount.get(i) == countOfColour){
+            //                 System.out.println("Colourrr : "+ colourList.get(i));
+            //                 System.out.println("INT VALUE: "+ i);
+            //                 flipCardsByColour(scoringZone, i);
+            //             }
+            //             i++;
+            //         }
+            //     }
+            // }
 
         }
     } 
 
-    // public Player determineWinner(){
-    //     return new Player();
-    // }
+    public void discardTwoCards(ArrayList<Player> players){
+        for (Player p : players){
+            // p.discardCard(); //Discards card chosen by player and do not return anything
+            // p.discardCard();
+            // p.handToScoringArea();
+        }
+    }
+
+    public Player determineWinner(){
+        //Count numbers in scoring zone
+        int lowest = 10000;
+        Player lowestcurrent = new HumanPlayer(new Deck(), "Temp");
+        for (Player p : players){
+            //System.out.println(p.getName());
+            int count = 0;
+            for (Card c : p.getCollectedParadeCards()){
+                //System.out.println(c.getFlipped());
+                if (c.getFlipped()){ //checked if flipped
+                    //System.out.println("Flipped Card");
+                    count++;
+                } else{
+                    count += c.getValue();
+                }
+            }
+            if (lowest > count){
+                lowest = count; 
+                lowestcurrent = p;
+            }
+            System.out.println(p.getName() + " Points: " + count);
+        }
+        return lowestcurrent;
+    }
 
 
 
@@ -303,14 +510,19 @@ public class Game {
         ArrayList<String> players = new ArrayList<>();
         players.add("Player 1");
         players.add("Player 2");
-        Game game = new Game(players);
+        players.add("Player 3");
+        players.add("Player 4");
+        Game game = new Game(players, true);
 
-        // while (game.gameEnd == false){
+        while (game.gameEnd == false){
             game.initiateRound();
+            System.out.println();
+            System.out.println();
             game.displayGameState(game);
-        // }
-        //game.initiateFinalRound();
-
+        }
+        game.initiateFinalRound(game.players);
+        game.displayGameState(game);
+        System.out.println("Winner is : " + game.determineWinner().getName());
         //new Card(2,"Red").printCard();
 
     }
