9f2dddb4f726e0a13a8a21962866de387a2df673
Christopher <legendarycte@gmail.com>
2025-03-29 14:57:46 +0800

    added 1 fullstop

--- src/app/resource/package-info.java
+++ src/app/resource/package-info.java
@@ -1,5 +1,5 @@
 /**
- * This package contains the resources classes such as cards and decks and the parade
+ * This package contains the resources classes such as cards and decks and the parade.
  * These classes includes the resources and attributes that allow provide functionality for Game to implement its logic. 
  */
 package app.resource;
